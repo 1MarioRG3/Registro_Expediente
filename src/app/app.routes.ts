7a10355189ec690a168286282c69cbd02a75d174
MarioRG <marioriverogarcia13@gmail.com>
2025-05-15 21:36:56 -0400

    main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { InfoComponent } from './info/info.component';
 
 export const routes: Routes = [
-  {path:'Registro_Expediente',component:HomeComponent},
-  {path:'Registro_Expediente/registrar',component:RegistrarComponent},
-  {path:'Registro_Expediente/info',component:InfoComponent}
+
+  {path:'',component:HomeComponent},
+  {path:'/registrar',component:RegistrarComponent},
+  {path:'/info',component:InfoComponent},
 ];
 
